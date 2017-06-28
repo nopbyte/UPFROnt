5a43ad1ddfe621b71e2beff1744e3c81522004f0
dschreckling <dschrecking@users.noreply.github.com>
2017-06-28 10:50:48 +0200

    Improve init process and adjust version

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 var pdp = require('./pdp');
+var w = require('winston');
+w.level = process.env.LOG_SERVER ? process.env.LOG_SERVER : "info";
 
+// TODO: Check for correct settings in pdp etc...
 function init(settings) {
     return new Promise(function(resolve, reject) {
-        pdp.init(settings).then(function() {
+        pdp.init(settings).then(function(fresh) {
+            if(!fresh)
+                w.warn("UPFROnt had already been initialized. Additional initialization has been skipped.");
+            else
+                w.info("UPFROnt initialized successfully.");
+
+            resolve();
+        }, function(e) {
+            reject(e);
+        });
+    });
+}
+
+function finish() {
+    return new Promise(function(resolve, reject) {
+        pdp.finish().then(function() {
             resolve();
         }, function(e) {
             reject(e);
@@ -11,8 +29,9 @@ function init(settings) {
 }
 
 module.exports = {
-    init : init,
-    pep : require("./pep"),
-    pdp : pdp,
-    pap : pdp.pap
+    init: init,
+    finish: finish,
+    pep: require("./pep"),
+    pdp: pdp,
+    pap: pdp.pap
 }
